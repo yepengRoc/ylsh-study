0620c372d46cd3ada3ad9ac62121a2d0cac9c703
yepengpeng <yepengpeng@chunbo.com>
2019-07-02 20:02:54 +0800

    2019-07-02 提交

--- src/main/java/com/ylsh/study/books/nettyqwzn/sty4/NettyTimeServer.java
+++ src/main/java/com/ylsh/study/books/nettyqwzn/sty4/NettyTimeServer.java
@@ -1,9 +1,51 @@
 package com.ylsh.study.books.nettyqwzn.sty4;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 public class NettyTimeServer {
 
     public void bind(int port){
 //        EventLoop
+        //reactor线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();//接收客户端连接
+        EventLoopGroup workGroup = new NioEventLoopGroup();//记性 socketchannel 网络的读写
 
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(bossGroup, workGroup)
+                .channel(NioServerSocketChannel.class)//对应jdk nio中的 serversocketchannel
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .childHandler(new ChildChannelHandler());//reactor 模式中的handler
+        try {
+            // 绑定端口，同步等待成功
+            ChannelFuture f = b.bind(port).sync();
+            //等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            //释放线程池资源
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args) {
+         int port = 8080;
+         new NettyTimeServer().bind(port);
     }
 }
---
+++ src/main/java/com/ylsh/study/books/nettyqwzn/sty4/TimeServerHandler.java
@@ -0,0 +1,34 @@
+package com.ylsh.study.books.nettyqwzn.sty4;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.Date;
+
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req,"UTF-8");
+        System.out.println("nio time server receive:" + body);
+        String curTime = new Date() + "";
+        ByteBuf resp = Unpooled.copiedBuffer(curTime.getBytes());
+        ctx.write(resp);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext channelHandlerContext) throws Exception {
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext channelHandlerContext, Throwable throwable) throws Exception {
+
+    }
+}
