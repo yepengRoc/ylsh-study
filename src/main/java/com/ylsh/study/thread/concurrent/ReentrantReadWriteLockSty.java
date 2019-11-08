b183f84a940be6e3ac3e8ce1ad512b0d8053c99b
yepengpeng <yepengpeng@chunbo.com>
2019-11-08 17:54:30 +0800

    2019-11-08 commit

--- src/main/java/com/ylsh/study/thread/concurrent/ReentrantReadWriteLockSty.java
+++ src/main/java/com/ylsh/study/thread/concurrent/ReentrantReadWriteLockSty.java
@@ -1,5 +1,6 @@
 package com.ylsh.study.thread.concurrent;
 
+import java.util.concurrent.atomic.LongAdder;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -10,5 +11,19 @@ public class ReentrantReadWriteLockSty {
 
         Lock readLock = rwLokc.readLock();
         Lock writeLock = rwLokc.writeLock();
+
+        readLock.lock();
+        readLock.unlock();
+        writeLock.lock();
+        writeLock.unlock();
+
+
+
+        LongAdder longAdder = new LongAdder();
+        longAdder.add(1L);
+        longAdder.add(2L);
+        longAdder.sum();
+
+
     }
 }
