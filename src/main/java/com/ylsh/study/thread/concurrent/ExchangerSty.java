ae22fa1e8a7f15833b646a7474708816bed6a319
yepengpeng <yepengpeng@chunbo.com>
2019-11-20 19:57:20 +0800

    2019-11-20 commit

--- src/main/java/com/ylsh/study/thread/concurrent/ExchangerSty.java
+++ src/main/java/com/ylsh/study/thread/concurrent/ExchangerSty.java
@@ -5,11 +5,15 @@ import java.util.concurrent.Exchanger;
 public class ExchangerSty {
 
     public static void main(String[] args)  throws  Exception{
+
+        System.out.println(0xff + 1);
         Exchanger<String> exchanger = new Exchanger();
 
         String val = "afdasd";
         val = exchanger.exchange(val);
         String val2 = null;
         val2 = exchanger.exchange(val2);
+
+
     }
 }
