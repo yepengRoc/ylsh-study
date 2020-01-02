409ae7e27af52f8533b4f8af18062389c19fab85
835431078@qq.com <835431078@qq.com>
2020-01-03 01:03:40 +0800

    2020-1-2 commit

--- src/main/java/com/ylsh/jvm/memory/Sty004.java
+++ src/main/java/com/ylsh/jvm/memory/Sty004.java
@@ -1,5 +1,8 @@
 package com.ylsh.jvm.memory;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+
 /**
  * 通过cglib创建一个个的类数据
  * -XX:MaxMetaspaceSize=10M
@@ -9,7 +12,19 @@ package com.ylsh.jvm.memory;
 public class Sty004 {
 
     public static void main(String[] args){
-//        Enhan
+
+        while(true){
+            Enhancer enhancer = new Enhancer();
+
+            enhancer.setSuperclass(Sty004.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback((MethodInterceptor)(obj, mehod, args1, proxy) ->{
+                proxy.invokeSuper(obj,args1);
+            });
+            System.out.println("cglib creat obj");
+            enhancer.create();
+        }
+
     }
 
     //图片 080630
